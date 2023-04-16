928df46d8deb0ec24eb49614c9f9a1b9bc3c1508
uniboy <liu.xinzh@northeastern.edu>
2023-04-15 23:27:50 -0400

    restore

--- v6.0/hardhat.config.ts
+++ v6.0/hardhat.config.ts
@@ -6,33 +6,41 @@ import "@openzeppelin/hardhat-upgrades";
 
 dotenv.config();
 
+const ALCHEMY_API_KEY = "Pg4OFD8wbiph6c6AAtraR4NcN4uKEdk8";
+
+const SEPOLIA_PRIVATE_KEY = "babe322664917e50558526a6619ce92abe809f9b77710aa25a8b52251149ea30";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   paths: {
     artifacts: './artifacts'
   },
   networks: {
-    hardhat: {
-      mining: {
-        auto: false,
-        interval: 10000
-      }
-    },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts:
-        process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY !== undefined
-          ? [process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY]
-          : []
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
     }
+    // hardhat: {
+    //   mining: {
+    //     auto: false,
+    //     interval: 10000
+    //   }
+    // },
+    // sepolia: {
+    //   url: process.env.ROPSTEN_URL || '',
+    //   accounts:
+    //     process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY !== undefined
+    //       ? [process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY]
+    //       : []
+    // }
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: 'USD'
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: 'USD'
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY
+  // }
 };
 
 export default config;
--- v6.0/scripts/deploy.ts
+++ v6.0/scripts/deploy.ts
@@ -1,19 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const Token = await ethers.getContractFactory("BasicDutchAuction");
+  const token = await Token.deploy(
+    1000,
+    10,
+    100,
   );
+
+  console.log("Token address:", token.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
